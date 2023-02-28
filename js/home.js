df22cd0cd2b8eb3d34b26a091dfb82497c6c2329
daltonjatc22 <114698602+daltonjatc22@users.noreply.github.com>
2023-02-28 13:38:47 -0700

    Fixed Planets

--- js/home.js
+++ js/home.js
@@ -1,69 +1,15 @@
+function movePlanets(newIndex) {
+    let targetValue = -100000;
+
+    $("main > .destination-wrapper").each((index, element) => {
+        console.log((targetValue*index)+"px");
+        element.style.setProperty("--z-height", (targetValue*(index-newIndex))+"px");
+    });
+}
+
 $(document).ready(function() {
     $(document).on("wheel", (e) => {
         console.log(e.originalEvent.deltaY);
     });
-    
-    // set init z height
-    
-    $("main > .destination-wrapper").each((index, element) => {
-        console.log(index);
-        element.style.transform = "translateZ("+(100000*index)+"px) translateY(0)";
-        element.style.zIndex = -index;
-    })
-    
-    // move planets to new Pos
-
-    
-    // set event listener for selector
-    /*
-    $(".destination-selector").children().click(function(event) {
-        event.stopPropagation();
-        console.log($(this).index());
-        movePlanets($(this).index());
-    });
-    */
+    movePlanets(0);
 });
-var currentIndex = 1;
-var currentOffset = 0;
-function movePlanets(newIndex) {
-    if(newIndex == currentIndex) return;
-    
-    $("main > .destination-wrapper").animate({
-        backgroundSize: '+=1'
-      },{
-        duration: 1000,
-        easing: "swing",
-        step: function(now,fx) {
-            now -= currentOffset;
-            $("main > .destination-wrapper").each((index, element) => {
-                element.style.transform = "translateZ("+(-1000*((index+currentIndex)*(now))-1000*((index+newIndex)*(1-now)))+"px) translateY(100px)";
-            });
-          }
-    },
-    ).promise().done(function(){
-        currentIndex = newIndex;
-        currentOffset++;
-    });
-    
-}
-
-
-function movePlanets2(newIndex) {
-    let targetValue = -10000;
-    $("main > .destination-wrapper").animate({
-        backgroundSize: '+=1'
-      },{
-        duration: 1000,
-        easing: "swing",
-        step: function(now,fx) {
-            now -= currentOffset;
-            $("main > .destination-wrapper").each((index, element) => {
-                element.style.transform = "translateZ("+(-1000*((index+currentIndex)*(now))-1000*((index+newIndex)*(1-now)))+"px) translateY(100px)";
-            });
-          }
-    },
-    ).promise().done(function(){
-        currentIndex = newIndex;
-        currentOffset++;
-    });
-}
